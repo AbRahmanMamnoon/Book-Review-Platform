ad4764b0b484de2b5ad7bcda2be21fa5c568c388
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-17 22:15:02 +0430

    server created

---
+++ server.js
@@ -0,0 +1,27 @@
+// packages
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// Utils
+import connectDB from './config/db.js';
+
+// Import routs
+import userRout from './routes/userRoutes.js';
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// Connect DB
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use('/api/v1/users', userRout);
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
