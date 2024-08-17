0948ebabc2aee60ed2659f7732b04f343d4e5c20
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-17 22:10:31 +0430

    User registration, login, logout, profile, and update user profile handler methods added

---
+++ controllers/userController.js
@@ -0,0 +1,119 @@
+import User from '../models/userModel.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
+import validMongoDBId from '../utils/validateMongoDBId.js';
+import generateToken from '../utils/generateToken.js';
+
+// Register User
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    throw new Error('Please fill all the inputs.');
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save();
+    generateToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// Login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (
+    existingUser &&
+    (await existingUser?.isPasswordValid(password, existingUser?.password))
+  ) {
+    generateToken(res, existingUser._id);
+    res.status(201).json({
+      _id: existingUser._id,
+      username: existingUser.username,
+      email: existingUser.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Pleas login');
+  }
+});
+
+// Logout current user
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'Logged out successfully' });
+});
+
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  validMongoDBId(_id);
+  const user = await User.findById(_id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.firstName = req.body.firstName || user.firstName;
+    user.lastName = req.body.lastName || user.lastName;
+    user.email = req.body.email || user.email;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      firstName: updatedUser.firstName,
+      lastName: updatedUser.lastName,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutCurrentUser,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+};
