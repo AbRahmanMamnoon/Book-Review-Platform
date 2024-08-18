1624506edbebd778b55d7da6ad0196f70df7965d
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-18 12:32:48 +0430

    book model created

---
+++ models/bookModel.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+// Declare the Book Schema
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    publishedDate: {
+      type: Date,
+    },
+    publisher: {
+      type: String,
+    },
+    pageCount: {
+      type: Number,
+    },
+    language: {
+      type: String,
+    },
+    coverImageUrl: {
+      type: String,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+export default mongoose.model('Book', bookSchema);
