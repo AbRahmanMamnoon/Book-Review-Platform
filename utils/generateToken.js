c347baa9a43f8a0322940b8cfc55610417db5358
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-18 12:31:05 +0430

    changes in generate token file like: secure true when in production, maxAge to 1day

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,11 +8,10 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: true,
-    sameSite: 'strict',
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV === 'production', // Secure in production, not in development
+    sameSite: 'strict', // or 'lax' if needed for third-party context
+    maxAge: 24 * 60 * 60 * 1000, // 01 day
   });
-
   return token;
 };
 
