6a2ce39b4d2b20f62d21b6f09605ef6d8284372a
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-19 15:30:30 +0430

    Created routes for getAllReviews searchReviewsByBook updateReview deleteReview searchReviewsByBook getAllReviews updateReview deleteReview

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,8 +2,20 @@ import express from 'express';
 const router = express.Router();
 
 import authenticate from '../middlewares/authMiddleware.js';
-import createReview from '../controllers/reviewController.js';
+import {
+  createReview,
+  getAllReviews,
+  searchReviewsByBook,
+  updateReview,
+  deleteReview,
+} from '../controllers/reviewController.js';
 
-router.route('/').post(authenticate, createReview);
+router.get('/search', searchReviewsByBook);
+router.route('/').get(getAllReviews).post(authenticate, createReview);
+
+router
+  .route('/:reviewId')
+  .patch(authenticate, updateReview)
+  .delete(authenticate, deleteReview);
 
 export default router;
