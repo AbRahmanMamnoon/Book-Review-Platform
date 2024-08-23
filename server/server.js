b494e768eb4198aafcac9dbb4193d1a23d0d9310
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-08-23 11:53:22 +0430

    Install security and logging middleware packages
    - Added 'morgan' for logging HTTP requests
    - Added 'helmet' to set various HTTP headers for security
    - Added 'hpp' to prevent HTTP parameter pollution
    - Added 'xss-clean' to sanitize user input against XSS attacks
    - Added 'express-mongo-sanitize' to prevent NoSQL injection
    - Added 'express-rate-limit' to limit repeated requests to public APIs

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import dataSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
 
 // Utils
 import connectDB from './config/db.js';
@@ -12,17 +18,72 @@ import userRout from './routes/userRoutes.js';
 import reviewRout from './routes/reviewRoutes.js';
 import bookRout from './routes/bookRoutes.js';
 
+// Start express here
+const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 // Connect DB
 connectDB();
 
-const app = express();
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Use helmet middleware to set up a Content Security Policy (CSP) for added security.
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:'],
+
+      fontSrc: ["'self'", 'https:', 'data:'],
+
+      scriptSrc: ["'self'", 'unsafe-inline'],
+
+      scriptSrc: ["'self'", 'https://*.cloudflare.com'],
+
+      scriptSrcElem: ["'self'", 'https:', 'https://*.cloudflare.com'],
+
+      styleSrc: ["'self'", 'https:', 'unsafe-inline'],
+
+      connectSrc: ["'self'", 'data', 'https://*.cloudflare.com'],
+    },
+  })
+);
+
+//Limit requests from same API
+const limiter = rateLimit({
+  max: 20,
+  windowMs: 50 * 60 * 1000,
+  message: 'Too many requests, please try again later.',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(dataSanitize());
+
+// Data sanitization against cross-site scripting
+app.use(xss());
+
+// Prevent parameters pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 // Configure CORS middleware
 app.use(
